5fe331b912be874dfd043472358bb920354a1c61
Aditya Khandelwal <aditya.khandelwal@mylofamily.com>
2022-02-11 12:48:09 +0530

    first commit

---
+++ lesson1.js
@@ -0,0 +1,6 @@
+// express = server-side, back-end framework
+// minimalistic, unopinionated and fast framework
+
+//Used to build web full stack apps
+
+// Middlwares - have access to req and response object, are used to perform some work, that is required in 
